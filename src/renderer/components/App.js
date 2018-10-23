a79841e62e0584d6dca29ba7b7b54188df2210d9
Michael Wooley <michaelwooley@users.noreply.github.com>
2018-10-23 06:34:19 -0500

    Showed how to abstract w/ alias.
    Using:
    https://github.com/parcel-bundler/parcel/pull/850

--- src/renderer/components/App.js
+++ src/renderer/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import logo from "./../../../assets/logo.svg";
+// import logo from "./../../../assets/logo.svg";
+import logo from "assets/logo.svg";
 
 class App extends Component {
   render() {
